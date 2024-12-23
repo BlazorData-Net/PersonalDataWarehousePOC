16ba8b23758d839152a028b47cdd48ae4dde5b4f
Michael Washington <webmaster@adefwebserver.com>
2024-12-22 22:05:35 -0800

    Default dynamic script working

--- PersonalDataWarehousePOC/Components/CodeExecution/DataLoader.cs
+++ PersonalDataWarehousePOC/Components/CodeExecution/DataLoader.cs
@@ -1,8 +1,30 @@
-﻿    public static class Dataloader
-    {
-    public static IEnumerable<IDictionary<string, object>> LoadParquet(string TableName)
+﻿using PersonalDataWarehousePOC.Services;
+using System.Data;
+using System.Linq;
+public class Dataloader
+{
+    public async Task<IEnumerable<IDictionary<string, object>>> LoadParquet(string TableName)
     {
         IEnumerable<IDictionary<string, object>> response = new List<IDictionary<string, object>>();
+
+        // Load the DataTable
+        var parquetFolder = Path.Combine("Data", "Parquet");
+        var fileName = Path.Combine(parquetFolder, $"{TableName}.parquet");
+
+        if (System.IO.File.Exists(fileName))
+        {
+            DataService objDataService = new DataService();
+            var CurrentDataTable = await objDataService.ReadParquetFileAsync(fileName);
+
+            // Convert the DataTable to a List of Dictionaries
+            response = CurrentDataTable.AsEnumerable()
+                .Select(row => CurrentDataTable.Columns
+                .Cast<DataColumn>()
+                .ToDictionary(column => column.ColumnName, column => row[column]))
+                .ToList();
+
+            return response;
+        }
 
         return response;
     }
