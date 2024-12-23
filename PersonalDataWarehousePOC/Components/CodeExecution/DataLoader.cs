deb86178e2a572c4a4c286f341c50f0b2a9cd020
Michael Washington <webmaster@adefwebserver.com>
2024-12-22 21:31:20 -0800

    Default script in progress

---
+++ PersonalDataWarehousePOC/Components/CodeExecution/DataLoader.cs
@@ -0,0 +1,9 @@
+﻿    public static class Dataloader
+    {
+    public static IEnumerable<IDictionary<string, object>> LoadParquet(string TableName)
+    {
+        IEnumerable<IDictionary<string, object>> response = new List<IDictionary<string, object>>();
+
+        return response;
+    }
+}
