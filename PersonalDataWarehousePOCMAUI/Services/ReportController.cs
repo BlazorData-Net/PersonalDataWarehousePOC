58e95b06a6a7b73d43f633e0f96365f43a272d61
Michael Washington <webmaster@adefwebserver.com>
2025-03-08 07:19:02 -0800

    Only allow RDL reports to be exported

--- PersonalDataWarehousePOCMAUI/Services/ReportController.cs
+++ PersonalDataWarehousePOCMAUI/Services/ReportController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Net;
 
 namespace PersonalDataWarehousePOCMAUI.Services
 {
+    // This attribute restricts access to localhost only
+    // It is applied to the entire controller 
+    // It checks the remote IP address of the request and returns a 403 Forbidden response if it's not localhost
     public class LocalhostOnlyAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext context)
@@ -31,7 +36,43 @@ namespace PersonalDataWarehousePOCMAUI.Services
         [HttpGet("/api/GetData")]
         public IActionResult GetData(string database, string datasource)
         {
-            return Ok(new { Database = database, Datasource = datasource });
+            var reportText = """"                
+                <Query>
+                <XmlData>
+                <Customers xmlns="http://www.adventure-works.com">  
+                   <Customer ID="11">  
+                      <FirstName>Bobby</FirstName>  
+                      <LastName>Moore</LastName>  
+                      <Orders>  
+                         <Order ID="1" Qty="6">Chair</Order>  
+                         <Order ID="2" Qty="1">Table</Order>  
+                      </Orders>  
+                      <Returns>  
+                         <Return ID="1" Qty="2">Chair</Return>  
+                      </Returns>  
+                   </Customer>  
+                   <Customer ID="20">  
+                      <FirstName>Crystal</FirstName>  
+                      <LastName>Hu</LastName>  
+                      <Orders>  
+                         <Order ID="8" Qty="2">Sofa</Order>  
+                      </Orders>  
+                      <Returns/>  
+                   </Customer>  
+                   <Customer ID="33">  
+                      <FirstName>Wyatt</FirstName>  
+                      <LastName>Diaz</LastName>  
+                      <Orders>  
+                         <Order ID="15" Qty="2">EndTables</Order>  
+                      </Orders>  
+                      <Returns/>  
+                   </Customer>  
+                </Customers>  
+                </XmlData>
+                </Query>
+                """";
+
+            return Ok(reportText);
         }
     }
 }
